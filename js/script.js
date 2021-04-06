13d71a5b8de1079a76ba0c6a556bce95c5e54df5
izzyraenaldy <35583421+izzyraenaldy27@users.noreply.github.com>
2021-04-07 00:39:26 +0700

    file portofolio website
    upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const txtElement = ['illustrat', 'Web Developer', 'Engineer'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words= '';
+
+(function ngetik(){
+	
+	if (count == txtElement.lenght){
+		 count = 0;
+	}
+
+	currentTxt = txtElement[count];
+
+
+
+	if(words.lenght == currentTxt.lenght){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 500);
+
+})(); 
