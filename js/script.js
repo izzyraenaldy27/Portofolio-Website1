fb69e02ae4e91409f1b54aeabbd50c0c6293ab93
izzyraenaldy <35583421+izzyraenaldy27@users.noreply.github.com>
2021-07-07 07:22:33 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
-const txtElement = ['illustrator', 'Web Developer', 'Engineer'];
+const txtElement = ['Kang Gabut', 'Programmer', 'Illustrator']
 let count = 0;
 let txtIndex = 0;
 let currentTxt = '';
-let words= '';
+let words = '';
 
 (function ngetik(){
-	
-	if (count == txtElement.lenght){
-		 count = 0;
-	}
+    if(count == txtElement.length){
+        count = 0;
+    }
+    currentTxt = txtElement[count];
 
-	currentTxt = txtElement[count];
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-ngetik').textContent = words;
 
+    if(words.length == currentTxt.length){
+        count++;
+        txtIndex = 0;
+    }
 
-
-	if(words.lenght == currentTxt.lenght){
-		count++;
-		txtIndex = 0;
-	}
-
-	setTimeout(ngetik, 500);
-
-})(); 
+    setTimeout(ngetik, 550);
+})();
